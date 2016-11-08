729621a719184425d56c9af8fda14e9940272bb0
Kimber <kimberly.winter@students.olin.edu>
2016-11-08 10:19:39 -0500

    Updated water droplet... A li'l better, but I'm still working on getting a continuous stream

--- waterdroplet.py
+++ waterdroplet.py
@@ -3,10 +3,11 @@
 import pygame, time
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
+screen = pygame.display.set_mode((640, 480))
 
 class droplet(object):
     def __init__(self):
-        self.radius = 1
+        self.radius = 20
         self.reset()
 
     def step(self):
@@ -14,39 +15,50 @@ class droplet(object):
         self.dy += -9
 
     def reset(self):
-        self.x = 320
         self.y = 0
         self.dy = -1
 
 
 class dropletView(object):
-    def __init__(self, model):
-        self.model = model
+    def __init__(self, nozzles):
+        self.nozzles = nozzles
+        for nozzle in self.nozzles:
 
     def draw(self, surface):
-        model = self.model
-        pygame.draw.circle(surface, BLUE, (model.x, model.y), model.radius)
-
-    def dropWater(self,)
-
+        for nozzle in self.nozzles:
+            for drop in nozzle.drops:
+                pygame.draw.circle(surface, BLUE, (nozzle.x, drop.y), drop.radius)
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((640, 480))
-    drop1 = droplet()
-    droplet_View = dropletView(drop1)
+    Testnozzle=nozzle(320)
+    Testnozzle1=nozzle(480)
+    droplet_View=dropletView([Testnozzle, Testnozzle1])
     while True:
-    	for event in pygame.event.get():
-    		if event.type == pygame.QUIT: 
-    			sys.exit()
-    	screen.fill(BLACK)
-    	drop1.step()
-    	droplet_View.draw(screen)
-    	pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                sys.exit()
+        droplet_View.draw(screen)
+        pygame.display.update()
+    #Testnozzle.x=320
+        Testnozzle.dropWater()
+        Testnozzle1.dropWater()
 
 class nozzle(object):
-	def __init__(self):
-		self.timeBetweenDrops =5
+    def __init__(self,x):
+        self.x=x
+        drop1=droplet()
+        self.drops=[drop1]
 
+    def dropWater(self):
+        screen.fill(BLACK)
+        for drop in self.drops:
+            drop.step()
+            print "HI"
+
+    def turn_on(self):
+        while(True):
+            self.drops.append(droplet())
+            
 
 main()
