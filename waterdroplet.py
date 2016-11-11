18c613f02e10ac518dc51e8801318e00888d312a
Kimber <kimberly.winter@students.olin.edu>
2016-11-10 20:00:10 -0500

    Not done yet, but the basic framework is there. #logicerrors

--- waterdroplet.py
+++ waterdroplet.py
@@ -3,16 +3,17 @@
 import pygame, time
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((1000,1000))
+
 
 class droplet(object):
     def __init__(self):
-        self.radius = 20
+        self.radius =1
         self.reset()
 
     def step(self):
         self.y += self.dy
-        self.dy += -9
+        self.dy += -1
 
     def reset(self):
         self.y = 0
@@ -22,27 +23,11 @@ class droplet(object):
 class dropletView(object):
     def __init__(self, nozzles):
         self.nozzles = nozzles
-        for nozzle in self.nozzles:
 
     def draw(self, surface):
         for nozzle in self.nozzles:
             for drop in nozzle.drops:
                 pygame.draw.circle(surface, BLUE, (nozzle.x, drop.y), drop.radius)
-
-def main():
-    pygame.init()
-    Testnozzle=nozzle(320)
-    Testnozzle1=nozzle(480)
-    droplet_View=dropletView([Testnozzle, Testnozzle1])
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
-                sys.exit()
-        droplet_View.draw(screen)
-        pygame.display.update()
-    #Testnozzle.x=320
-        Testnozzle.dropWater()
-        Testnozzle1.dropWater()
 
 class nozzle(object):
     def __init__(self,x):
@@ -54,11 +39,28 @@ class nozzle(object):
         screen.fill(BLACK)
         for drop in self.drops:
             drop.step()
-            print "HI"
 
     def turn_on(self):
-        while(True):
-            self.drops.append(droplet())
-            
+        self.drops.append(droplet())
+
+
+pygame.init()
+pygame.display.set_caption("Water Simulation")
+Testnozzle=nozzle(320)
+Testnozzle1=nozzle(480)
+droplet_View=dropletView([Testnozzle, Testnozzle1])
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                sys.exit()
+        droplet_View.draw(screen)
+        pygame.display.update()
+    #Testnozzle.x=320
+        Testnozzle.turn_on()
+        Testnozzle.dropWater()
+        Testnozzle1.dropWater()
+
 
 main()
